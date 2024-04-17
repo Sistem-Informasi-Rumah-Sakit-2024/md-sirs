7bbdb34b32603ba45f58f985c5a45e8ab845dd6d
Arrahmanul Hakim <hakimarrahmannul@gmail.com>
2024-04-17 23:54:23 +0700

    main stream auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sirs/app/controllers/auth_controller.dart';
+import 'package:sirs/app/utils/loading.dart';
 import 'app/routes/app_pages.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -9,11 +12,28 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: Routes.LOGIN,
-      getPages: AppPages.routes,
-    ),
-  );
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final autC = Get.put(AuthController(), permanent: true);
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: autC.streamAuthStatus,
+      builder: (context, snapshot) {
+        print('SNAPSHOT ${snapshot.data}');
+        if (snapshot.connectionState == ConnectionState.active) {
+          return GetMaterialApp(
+            title: "Application",
+            initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
+            getPages: AppPages.routes,
+            // home: snapshot.data != null ? HomeView() : LoginView(),
+          );
+        }
+        return const LoadingView();
+      },
+    );
+  }
 }
