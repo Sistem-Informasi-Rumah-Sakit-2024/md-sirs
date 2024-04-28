64642457b546c269ac8b6c46cac687ae3e0620c2
Arrahmanul Hakim <hakimarrahmannul@gmail.com>
2024-04-28 17:13:36 +0700

    Refactor app_routes.dart to add Records module and update imports

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,4 +1,6 @@
 import 'package:get/get.dart';
+import 'package:sirs/app/modules/records/records/bindings/records_binding.dart';
+import 'package:sirs/app/modules/records/records/views/records_view.dart';
 
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
@@ -6,10 +8,8 @@ import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
 import '../modules/profile/bindings/profile_binding.dart';
 import '../modules/profile/views/profile_view.dart';
-import '../modules/records/bindings/records_binding.dart';
 import '../modules/records/record_actions/bindings/record_actions_binding.dart';
 import '../modules/records/record_actions/views/record_actions_view.dart';
-import '../modules/records/views/records_view.dart';
 
 part 'app_routes.dart';
 
