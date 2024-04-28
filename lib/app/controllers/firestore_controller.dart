4434fe6f69a26235030017e331471c3fdba48920
Arrahmanul Hakim <hakimarrahmannul@gmail.com>
2024-04-28 17:13:00 +0700

    Add Firestore CRUD operations and record statistics

--- lib/app/controllers/firestore_controller.dart
+++ lib/app/controllers/firestore_controller.dart
@@ -3,9 +3,75 @@ import 'package:get/get.dart';
 
 class FirestoreController extends GetxController {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
-  // FirebaseFirestore.instance
-  // .collection('scores')
-  // .orderBy('score', descending: true)
+
+  //TODO EDIT RECORD
+  void editRecord(String recordId, Map<String, dynamic> dataToUpdate) {
+    firestore
+        .collection("records_data")
+        .doc(recordId)
+        .update(dataToUpdate)
+        .then((_) {})
+        .catchError((error) {
+      Get.showSnackbar(GetBar(
+        message: "Failed to update record: $error",
+        duration: const Duration(seconds: 3),
+      ));
+    });
+  }
+
+  //TODO: DELETE RECORD
+  Future<void> deleteDocument(String recordId) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection("records_data")
+          .doc(recordId)
+          .delete();
+      print("Document deleted successfully!");
+    } catch (e) {
+      print("Error deleting document: $e");
+      // Handle specific exceptions if needed
+    }
+  }
+
+  //TODO: Get length records_data
+  Future<int> uniquePatientIndexCount() async {
+    QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('records_data').get();
+
+    // Create an empty set to store unique patient_index values
+    Set<String> patientIndexSet = Set<String>();
+
+    // Loop through the documents and add each "patient_index" value to the set
+    snapshot.docs.forEach((doc) {
+      patientIndexSet.add(doc['patient_index']);
+    });
+
+    // Return the size of the set, which represents the number of unique patient_index values
+
+    return patientIndexSet.length;
+  }
+
+  Future<int> totalRecords() async {
+    QuerySnapshot snapshot = await firestore.collection('records_data').get();
+    return snapshot.docs.length;
+  }
+
+  Future<int> totalRecordsMale() async {
+    QuerySnapshot snapshot = await firestore
+        .collection('records_data')
+        .where('sex', isEqualTo: 'M')
+        .get();
+    return snapshot.docs.length;
+  }
+
+  Future<int> totalRecordsFemale() async {
+    QuerySnapshot snapshot = await firestore
+        .collection('records_data')
+        .where('sex', isEqualTo: 'F')
+        .get();
+    return snapshot.docs.length;
+  }
+
   Query<Object?> query = FirebaseFirestore.instance
       .collection('records_data')
       .orderBy('age', descending: false);
