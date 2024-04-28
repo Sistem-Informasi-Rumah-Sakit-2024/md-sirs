b486f59e804b5f625f629e1afbd9e3500a1b8611
Arrahmanul Hakim <hakimarrahmannul@gmail.com>
2024-04-28 17:14:22 +0700

    Refactor main.dart to use StreamBuilder for authentication status

--- lib/main.dart
+++ lib/main.dart
@@ -20,26 +20,27 @@ class MyApp extends StatelessWidget {
   final autC = Get.put(AuthController(), permanent: true);
   @override
   Widget build(BuildContext context) {
-    // return StreamBuilder<User?>(
-    //   stream: autC.streamAuthStatus,
-    //   builder: (context, snapshot) {
-    //     print('SNAPSHOT ${snapshot.data}');
-    //     if (snapshot.connectionState == ConnectionState.active) {
-    //       return GetMaterialApp(
-    //         title: "Application",
-    //         initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
-    //         getPages: AppPages.routes,
-    //         // home: snapshot.data != null ? HomeView() : LoginView(),
-    //       );
-    //     }
-    //     return const LoadingView();
-    //   },
-    // );
-    return GetMaterialApp(
-      title: "Application",
-      initialRoute: Routes.HOME,
-      getPages: AppPages.routes,
-      // home: snapshot.data != null ? HomeView() : LoginView(),
+    return StreamBuilder<User?>(
+      stream: autC.streamAuthStatus,
+      builder: (context, snapshot) {
+        print('SNAPSHOT ${snapshot.data}');
+        if (snapshot.connectionState == ConnectionState.active) {
+          return GetMaterialApp(
+            title: "Application",
+            initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
+            getPages: AppPages.routes,
+            // home: snapshot.data != null ? HomeView() : LoginView(),
+          );
+        }
+        return const LoadingView();
+      },
     );
+    // return GetMaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   title: "Application",
+    //   initialRoute: Routes.HOME,
+    //   getPages: AppPages.routes,
+    //   // home: snapshot.data != null ? HomeView() : LoginView(),
+    // );
   }
 }
