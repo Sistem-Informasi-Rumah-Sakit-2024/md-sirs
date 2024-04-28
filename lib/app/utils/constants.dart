cbbf2fb4dfa9212fb96018dd1eb0682a69d5e1a9
Arrahmanul Hakim <hakimarrahmannul@gmail.com>
2024-04-28 17:10:32 +0700

    Add PageType constants for different page types

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -7,3 +7,10 @@ const Color kPrimaryDark200 = Color(0xFF282828);
 const Color kPrimaryDark300 = Color(0xFF3f3f3f);
 // const Color kPrimaryblue500 = Color(0xFFa688fa);
 const Color kPrimaryblue500 = Color(0xFF6F35A5);
+
+abstract class PageType {
+  PageType._();
+  static const TAMBAH = 0;
+  static const EDIT = 1;
+  static const DETAIL = 2;
+}
